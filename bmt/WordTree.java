d39839e6a3f2fc4fb29fd24b1f8527b31b789287
Jacob Mckenna <jkmckenna94@gmail.com>
2018-06-04 02:44:25 +1200

    Initial Javadoc
    Adding comments for Javadoc stuff

--- bmt/WordTree.java
+++ bmt/WordTree.java
@@ -2,6 +2,12 @@ package bmt;
 
 import java.util.ArrayList;
 
+/**
+* This WordTree class acts as a radix trie for storing words.<br>
+* It has a few tools to make it simpler to define words and prefixes/sufixes.<br>
+*
+* @author Jacob Mckenna
+*/
 public class WordTree {
     private class Node {
         private int depth;
@@ -34,7 +40,15 @@ public class WordTree {
     public WordTree() {
         this.rootNode = new Node();
     }
-
+    
+    /**
+    * Inserts a {@code string} into the WordTree.
+    * It returns a boolean denoting whether the word has been added or not.
+    *
+    * @param word the {@code string} to be insterted into the WordTree
+    * @return {@code true} if word has been added.<br>
+    *         {@code false} if word hasn't been added or it already exists. 
+    */
     public boolean addWord(String word) {
         return false;
     }
