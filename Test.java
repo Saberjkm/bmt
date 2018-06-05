1e205ea53fd14c9c8486f2f5741fcc9820354b37
Jacob Mckenna <jkmckenna94@gmail.com>
2018-06-05 20:00:43 +1200

    Added functionality for adding a word
    Started the process of adding a word, it should work but needs some test methods

--- Test.java
+++ Test.java
@@ -3,6 +3,10 @@ import bmt.*;
 public class Test {
     public static void main(String... args) {
         WordTree testTree = new WordTree();
+        System.out.println(testTree.addWord("hello"));
+        System.out.println(testTree.addWord("hello  "));
+        System.out.println(testTree.addWord("hell"));
+        System.out.println(testTree.addWord("hell"));
         System.out.println("Hello World");
     }
 }
--- bmt/WordTree.java
+++ bmt/WordTree.java
@@ -16,7 +16,7 @@ public class WordTree {
         private ArrayList<Node> childNodes;
 
         public Node() {
-            this.depth = 0;
+            this.depth = -1;
             this.isEndOfWord = false;
             this.representedCharacter = 0;
             this.childNodes = new ArrayList<>();
@@ -29,10 +29,55 @@ public class WordTree {
             this.childNodes = new ArrayList<>();
         }
 
-        public boolean isEndOfWord() { return this.isEndOfWord; }
+        public Node(char character, int depth, boolean end) {
+            this.depth = depth;
+            this.representedCharacter = character;
+            this.isEndOfWord = end;
+            this.childNodes = new ArrayList<>();
+        }
 
+        public boolean isEndOfWord() { return this.isEndOfWord; }
+        public void setEndOfWord(boolean bool) { this.isEndOfWord = bool; }
+ 
         public char getCharacter() { return this.representedCharacter; }
         public void setCharacter(char character) { this.representedCharacter = character; }
+
+        // Not sure if i really need to carry around the hasAdded bool
+        public boolean addWord(String word, boolean hasAdded) {
+            if (word.length() == 0) { return hasAdded; } 
+            Node testNode = this.contains(word.charAt(0));
+            // If testNode is null this means that its a new word to be added so process it
+            if (testNode == null) {
+                // If only a single character left then add an end of word node
+                if (word.length() == 1) {
+                    this.childNodes.add(new Node(word.charAt(0), this.depth + 1, true));
+                    return true;
+                } else {
+                    Node newNode = new Node(word.charAt(0), this.depth + 1, false);
+                    this.childNodes.add(newNode);
+                    return (newNode.addWord(word.substring(1), true));
+                }
+            } else {
+                // If its the last characte in <word> check if the node is designated as an end of word character.
+                if (word.length() == 1) {
+                    if (testNode.isEndOfWord()) { 
+                        return hasAdded;
+                    } else {
+                        testNode.setEndOfWord(true);
+                        return true;
+                    }
+                } else {
+                    return (testNode.addWord(word.substring(1), hasAdded));
+                }
+            }
+        }
+
+        public Node contains(char character) {
+            for (Node node : this.childNodes) {
+                if (node.getCharacter() == character) { return node; }
+            }
+            return null;
+        }
     }
 
     private Node rootNode;
@@ -44,13 +89,14 @@ public class WordTree {
     /**
     * Inserts a {@code string} into the WordTree.
     * It returns a boolean denoting whether the word has been added or not.
+    * Also trims whitespace and converts to lowercase.
     *
-    * @param word the {@code string} to be insterted into the WordTree
+    * @param word the {@code string} to be inserted into the WordTree
     * @return {@code true} if word has been added.<br>
     *         {@code false} if word hasn't been added or it already exists. 
     */
     public boolean addWord(String word) {
-        return false;
+        return rootNode.addWord(word.trim().toLowerCase(), false); 
     }
     
 }
